cc0bd880b3b266ebbf8a7a4f707b00c5db3b1a98
unknown <aswalshrey@gmail.com>
2023-01-03 22:21:32 +0530

    Display TimeTable

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import React,{useEffect, useRef, useState} from 'react';
 import './App.css';
 
 function App() {
+
+  // const initialValue =""
+  const inputRef = useRef()
+
+  useEffect(() => {
+    inputRef.current.focus()
+  })
+
+  const focusBack = () => {
+    setValue(inputRef.current.focus())
+  }
+
+  const [value, setValue] = useState("")
+  const [allValue,setAllValue] =useState([])
+
+  const change = (e) => {
+    // e.preventDefault()
+    setValue(e.target.value)
+  }
+
+  const showCase = (e) => {
+    if(e){
+    setAllValue(prevS=>[...prevS,value])
+    setValue("")
+    }
+  }
+
+  const changeText = (alt,id ) => {
+    if(document.getElementById(`${id}`).innerHTML===alt){
+      document.getElementById(`${id}`).innerHTML="Text is Private"
+      document.getElementById(`${id}`).style.color="white"
+
+      console.log(alt)
+
+    }
+    else{
+      document.getElementById(`${id}`).innerHTML=alt;
+      document.getElementById(`${id}`).style.color = "black"
+      console.log(alt + "anmol")
+
+    }
+  }
+
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Activities</h1>
+      <input ref={inputRef} type='text' value={value} onChange={(e)=>change(e)}></input>
+      <button onClick={()=>showCase(value)} >Add</button>
+      <div>
+        {
+        allValue.map((alt,index) => {
+          return (
+            <div>
+              <ul style={{backgroundColor:'peachpuff'}}>
+                <li id={`${index}`} style={{color:'black'}}>
+                  {alt}
+                </li>
+                <button onClick={() => changeText(alt,index) } >toggle</button>
+              </ul>
+            </div>
+          )
+        })
+        }
+      </div>
+      <button onClick={focusBack}> Focus</button>
     </div>
   );
+
 }
 
 export default App;
+
+
+
+// const reducer = (state, action) => {
+  //   switch (action) {
+  //     case 'add' :
+  //       return state
+  //   }
+  // }
+
+  // const [sentence,dispatch] = useReducer(reducer,initialValue)
